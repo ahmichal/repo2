1d4d676af9bf9facf9d7b5581e595d255206da16
ahmichal <95378632+ahmichal@users.noreply.github.com>
2022-03-07 09:31:45 +0200

    "second commit - spelling check"

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 		for (int i=1;i<=10;i++){
-		System.out.println("Hellow World..."+i);
+		System.out.println("Hello World... but now its spelled correctly"+i);
 		}
 	}
 }
